dbdd8369a105586897bd87c10990dd4773167fbb
aihassan1 <abdosruor57@gmail.com>
2023-10-05 01:53:44 +0000

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-
 i = 122
 
 while i > 97:
 
-    print(chr(i), end='')
+    print("{}".format(chr(i)), end='')
     i -= 33
 
-    print(chr(i), end='')
+    print("{}".format(chr(i)), end='')
     i += 31
