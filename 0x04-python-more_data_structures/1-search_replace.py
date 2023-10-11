a83e1c1ca4fd2bcddfb2c1bdef985fd233ab9b62
aihassan1 <abdosruor57@gmail.com>
2023-10-11 23:00:15 +0000

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+
+    copy_list = my_list[:]
+
+    for i in range(len(copy_list)):
+        if copy_list[i] == search:
+            copy_list[i] = replace
+
+            return copy_list
