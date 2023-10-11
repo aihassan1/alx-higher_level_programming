6e624f63f46bdf233b6cd115334b0ccf4d6ccca4
aihassan1 <abdosruor57@gmail.com>
2023-10-11 22:45:21 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,8 +3,7 @@
 
 def square_matrix_simple(matrix=[]):
     copied_matrix = [row[:] for row in matrix]
-    
+
     edited_matrix = [[x**2 for x in row] for row in copied_matrix]
 
     return edited_matrix
-                
