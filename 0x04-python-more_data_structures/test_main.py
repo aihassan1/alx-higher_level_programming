c066fc803cfd60e44331b81b61aff69d16f67199
aihassan1 <abdosruor57@gmail.com>
2023-10-12 07:50:22 +0000

    6-print_sorted_dictionary.pyaaaaaaaaaaaaa

---
+++ 0x04-python-more_data_structures/test_main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+print_sorted_dictionary = \
+        __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+my_dict = { 'a': "e", 'B': "d" , 'c': "c", 'd': "b", 'E': "a" }
+print_sorted_dictionary(my_dict)
