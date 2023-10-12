a3446588197571e2bf63f7f7f83a0e7c869e1943
aihassan1 <abdosruor57@gmail.com>
2023-10-12 11:56:02 +0000

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_num_list = list(roman_string)
+    total = 0
+
+    for i in range(len(roman_num_list)):
+
+        number = roman.get(roman_num_list[i])
+        if number is None:
+            return 0
+
+        if i < len(roman_num_list) - 1:
+            next_num = roman.get(roman_num_list[i + 1])
+            if next_num > number:
+                number = -number
+
+        total += number
+
+    return total
