a8461c9cf86492524106ca22529fa717ccc54fe2
aihassan1 <abdosruor57@gmail.com>
2023-10-12 08:52:13 +0000

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+
+    if a_dictionary is not None:
+
+        highest_key = max(a_dictionary)
+        return highest_key
+
+    else:
+        return None
