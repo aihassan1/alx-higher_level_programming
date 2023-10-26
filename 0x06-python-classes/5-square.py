522ce91ea4943b1bc851375c97994562f1e4a805
aihassan1 <abdosruor57@gmail.com>
2023-10-26 02:07:41 +0000

    5-square.pyaaaa

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -12,24 +12,13 @@ class Square:
 
     def __init__(self, size=0):
         """
-<<<<<<< HEAD
-         Initializes a new Square instance with a given size.
-
-         Args:
-            size (int): The size of the square.
-
-        """
-
-        self.__size = size
-=======
         Initializes a new Square instance with a given size.
 
         Args:
             size (int): The size of the square.
 
         """
-        self.__size = size 
->>>>>>> 7c5b186bd62425b1f4c0cef2376faafb8dc7d987
+        self.__size = size
 
     @property
     def size(self):
@@ -45,11 +34,7 @@ class Square:
     @size.setter
     def size(self, value):
         """
-<<<<<<< HEAD
-            Setter method for the size property.
-=======
         Setter method for the size property.
->>>>>>> 7c5b186bd62425b1f4c0cef2376faafb8dc7d987
 
         Args:
             value (int): The new size of the square.
@@ -62,16 +47,6 @@ class Square:
         if isinstance(value, int):
             if value >= 0:
                 self.__size = value
-<<<<<<< HEAD
-        else:
-            raise ValueError("size must be >= 0")
-        else:
-            raise TypeError("size must be an integer")
-
-        def area(self):
-            """
-            Calculates the area of the square.
-=======
             else:
                 raise ValueError("size must be >= 0")
         else:
@@ -80,26 +55,10 @@ class Square:
     def area(self):
         """
         Calculates the area of the square.
->>>>>>> 7c5b186bd62425b1f4c0cef2376faafb8dc7d987
 
         Returns:
             int: The area of the square.
 
-<<<<<<< HEAD
-            """
-            return self.__size * self.__size
-
-        def my_print(self):
-            """prints a square of # based on size """
-            if self.__size != 0:
-                for i in range(self.size):
-                    for j in range(self.size):
-                        print("#", end='')
-                    print()
-
-                    else:
-                        print()
-=======
         """
         return self.__size * self.__size
 
@@ -108,11 +67,8 @@ class Square:
         if self.__size != 0:
             for i in range(self.size):
                 for j in range(self.size):
-                    print("#",end='')                   
+                    print("#", end='')
                 print()
 
         else:
             print()
-
-
->>>>>>> 7c5b186bd62425b1f4c0cef2376faafb8dc7d987
