9c8a37aa70e2a97f44f1a00f476f7814817284c7
aihassan1 <abdosruor57@gmail.com>
2023-10-31 02:05:58 +0000

    7-rectangle.py

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+""" create a class for a rec"""
+
+
+class Rectangle:
+    """creates a rec"""
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        """initilize parameters """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """get width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """height setter """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """defines area of rec"""
+        area = (self.width * self.height)
+        return (area)
+
+    def perimeter(self):
+        """calculates para of rec"""
+        if self.width == 0 or self.height == 0:
+            return (0)
+        para = (2 * (self.width + self.height))
+        return (para)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rectangle_str = "\n".join([str(self.print_symbol) * self.__width] *
+                                  self.__height)
+        return rectangle_str
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        if Rectangle.number_of_instances != 0:
+            Rectangle.number_of_instances -= 1
+
+        print("Bye rectangle...")
