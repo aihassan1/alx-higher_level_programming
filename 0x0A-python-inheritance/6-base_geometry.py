385f5d47bd74ab7f9bb979e46f0a881fdfaa15ec
aihassan1 <abdosruor57@gmail.com>
2023-11-07 01:23:55 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module for base geometry. """
+
+
+class BaseGeometry:
+    """ Base class for geometric shapes."""
+
+    def area(self):
+        """calculates area - not defined yet"""
+        raise Exception("area() is not implemented")
