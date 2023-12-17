756127d1b0484ba1db449e50e7a166c6cb4271d2
aihassan1 <abdosruor57@gmail.com>
2023-12-17 16:29:10 +0000

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,7 +2,7 @@
 
 -- Each record should display: cities.id - cities.name - states.name
 
-SELECT cities.id AS city_id , cities.name AS city_name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities 
 LEFT JOIN states
 ON cities.state_id = states.id;
