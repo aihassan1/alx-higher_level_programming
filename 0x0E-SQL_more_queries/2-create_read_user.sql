72ea6bb6e22bb66860294bd7642a604690ae16d7
aihassan1 <abdosruor57@gmail.com>
2023-12-17 01:30:53 +0000

    2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- 2-create-database-user.sql
+
+-- Create the database hbtn_0d_2 if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+
+-- Create the user user_0d_2 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on hbtn_0d_2 to user_0d_2
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
