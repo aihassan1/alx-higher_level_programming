a3dfd38b64de3a8cd4f038132c5a72404b3fb180
aihassan1 <abdosruor57@gmail.com>
2024-01-10 14:42:10 +0200

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// a
+
+let i, j;
+const firstArg = process.argv[2];
+const size = parseInt(firstArg);
+if (isNaN(size)) {
+  console.log('Missing size');
+}
+
+for (i = 0; i < size; i++) {
+  let row = '';
+  for (j = 0; j < size; j++) {
+    row += 'x';
+  }
+  console.log(row);
+}
