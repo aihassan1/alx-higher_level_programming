d98f6230c276276bae497eebc0c7bb5a493fd81f
aihassan1 <abdosruor57@gmail.com>
2024-01-17 16:04:33 +0200

    1-filter_states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""document the file 1-filter_states.py"""
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-
+"""0-select states"""
 import MySQLdb
 import sys
 
-""" Retrieve MySQL username, password,
-and database name from command line arguments"""
-username = sys.argv[1]
-password = sys.argv[2]
-database_name = sys.argv[3]
-
 if __name__ == "__main__":
-    """Ensure code is not executed when imported"""
-    # Connect to the MySQL database
+    """code should not be executed when imported"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
     db = MySQLdb.connect(host="localhost", user=username, db=database_name)
+
     cursor = db.cursor()
 
-    # Execute SQL query to select states starting with 'N'
-    cursor.execute
-    ("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
-
-    # Fetch all rows and print them
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
-    # Close the cursor and database connection
     cursor.close()
     db.close()
--- 0x0F-python-object_relational_mapping/test2.py
+++ 0x0F-python-object_relational_mapping/test2.py
@@ -1,4 +1,28 @@
-import sys
 
-for i in range(len(sys.argv)):
-    print(i,sys.argv[i])
+
+#!/usr/bin/python3
+"""document the file 1-filter_states.py"""
+import MySQLdb
+import sys
+username = sys.argv[1]
+password = sys.argv[2]
+database_name = sys.argv[3]
+
+if __name__ == "__main__":
+    """Ensure code is not executed when imported"""
+    # Connect to the MySQL database
+    db = MySQLdb.connect(host="localhost", user=username, db=database_name)
+    cursor = db.cursor()
+
+    # Execute SQL query to select states starting with 'N'
+    cursor.execute
+    ("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+
+    # Fetch all rows and print them
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
