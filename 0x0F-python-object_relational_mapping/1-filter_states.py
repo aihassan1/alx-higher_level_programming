4c20b6cc3fd57760c9bb46556b1222407bab6fd6
aihassan1 <abdosruor57@gmail.com>
2024-01-17 15:59:06 +0200

    1-filter_states.pyyyy

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,19 +3,21 @@
 import MySQLdb
 import sys
 
-# Retrieve MySQL username, password, and database name from command line arguments
+""" Retrieve MySQL username, password,
+and database name from command line arguments"""
 username = sys.argv[1]
 password = sys.argv[2]
 database_name = sys.argv[3]
 
 if __name__ == "__main__":
-    # Ensure code is not executed when imported
+    """Ensure code is not executed when imported"""
     # Connect to the MySQL database
     db = MySQLdb.connect(host="localhost", user=username, db=database_name)
     cursor = db.cursor()
 
     # Execute SQL query to select states starting with 'N'
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cursor.execute
+    ("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
 
     # Fetch all rows and print them
     rows = cursor.fetchall()
