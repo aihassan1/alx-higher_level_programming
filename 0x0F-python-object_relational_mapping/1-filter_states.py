c685c146ce486cc88d784619f61071f378c756d5
aihassan1 <abdosruor57@gmail.com>
2024-01-17 15:54:29 +0200

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+import MySQLdb
+import sys
+
 """
 Filter states : script that lists all states with a name starting with N
 (upper N) from the database hbtn_0e_0_usa
 """
-import MySQLdb
-import sys
-
 username = sys.argv[1]
 password = sys.argv[2]
 database_name = sys.argv[3]
