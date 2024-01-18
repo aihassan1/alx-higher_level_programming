4a18aab67536652ac4e6508732bfd2a117c456db
aihassan1 <abdosruor57@gmail.com>
2024-01-19 01:57:10 +0200

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-script that lists all State objects from the database hbtn_0e_6_usa.
+"""script that lists all State objects from the database hbtn_0e_6_usa.
 """
 
 import sys
