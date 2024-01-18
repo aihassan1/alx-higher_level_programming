ffd7a14b3f52cb7d0f7b002bc736dc1ca301949a
aihassan1 <abdosruor57@gmail.com>
2024-01-19 00:51:27 +0200

    6-model_state.

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" class definition of a State"""
+
+from sqlalchemy import create_engine, Column, String, CHAR, Integer, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+engine = create_engine(
+    "mysql+mysqldb://root@localhost:3360/hbtn_0e_6_usa", echo=True)
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(
+        "id", Integer, primary_key=True,
+        autoincrement=True, nullable=False, unique=True
+    )
+    name = Column("name", CHAR(128), nullable=False)
+
+
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+session.close()
