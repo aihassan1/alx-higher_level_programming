d3c1dc37a9f61894101f8baed105ded754784a7d
aihassan1 <abdosruor57@gmail.com>
2024-01-18 02:44:02 +0200

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -7,5 +7,3 @@ CREATE TABLE IF NOT EXISTS states (
     PRIMARY KEY (id)
 );
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
---@block
-SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC
