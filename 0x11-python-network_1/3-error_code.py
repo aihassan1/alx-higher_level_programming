92a35e2a67b2cfbd1489f22813882194f047fdcc
aihassan1 <abdosruor57@gmail.com>
2024-01-30 23:52:51 +0200

    3333

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,21 +6,23 @@ import sys
 import urllib.request
 import urllib.error
 
-# Retrieve the URL from the command-line arguments
-url = sys.argv[1]
+if __name__ == "__main__":
 
-# Create a urllib.request.Request object with the provided URL
-request = urllib.request.Request(url)
+    # Retrieve the URL from the command-line arguments
+    url = sys.argv[1]
 
-try:
-    # Open the URL and read the response
-    with urllib.request.urlopen(request) as response:
-        # Decode the response body as UTF-8
-        body = response.read().decode("utf-8")
-        # Print the body of the response
-        print(body)
+    # Create a urllib.request.Request object with the provided URL
+    request = urllib.request.Request(url)
 
-# Handle HTTP errors
-except urllib.error.HTTPError as e:
-    # Print the error code
-    print("Error code: {}".format(e.code))
+    try:
+        # Open the URL and read the response
+        with urllib.request.urlopen(request) as response:
+            # Decode the response body as UTF-8
+            body = response.read().decode("utf-8")
+            # Print the body of the response
+            print(body)
+
+    # Handle HTTP errors
+    except urllib.error.HTTPError as e:
+        # Print the error code
+        print("Error code: {}".format(e.code))
