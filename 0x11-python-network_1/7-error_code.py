d7bce4cdac86e4f60734f362a70848cf65ac1702
aihassan1 <abdosruor57@gmail.com>
2024-01-31 02:28:22 +0200

    7-error_code.pyy

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,13 +5,13 @@ Displays the body of the response (decoded in utf-8)
 import sys
 import requests
 
-if __name__ == "__main__":
-    # Retrieve the URL from the command-line arguments
-    url = sys.argv[1]
 
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+# Retrieve the URL from the command-line arguments
+url = sys.argv[1]
 
-    else:
-        print(r.content)
+r = requests.get(url)
+if r.status_code >= 400:
+    print("Error code: {}".format(r.status_code))
+
+else:
+    print(r.text)
