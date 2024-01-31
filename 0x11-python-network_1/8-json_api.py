043e39a0314bdabdd410b1e02fa7e6c29cb005eb
aihassan1 <abdosruor57@gmail.com>
2024-01-31 02:58:47 +0200

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""sends a POST request to url with the letter as a parameter."""
+
+import requests
+import sys
+
+letter = "" if len(sys.argv) == 1 else sys.argv[1]
+parameter = {"q": letter}
+
+response = requests.post("http://0.0.0.0:5000/search_user", data=parameter)
+try:
+    json_data = response.json()
+
+    if json_data == {}:
+        print("No result")
+    else:
+        id = json_data.get("id")
+        name = json_data.get("name")
+        print("[{}] {}".format(id, name))
+
+except ValueError:
+    print("Not a valid JSON")
