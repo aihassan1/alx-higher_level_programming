5e0c2e915742b024fdfc8c4cf5b47d746cc07d65
aihassan1 <abdosruor57@gmail.com>
2024-01-31 02:11:13 +0200

    7-error_code.pyaaa

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url=url)
-    if r.status_code < 400:
-        print(r.text)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
 
     else:
-        print("Error code: {}".format(r.status_code))
+        print(r.text)
