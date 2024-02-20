6ecd79d96228dcc24a62e1b375c77848e6baca6f
aihassan1 <abdosruor57@gmail.com>
2024-02-21 00:16:08 +0200

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,18 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const url = process.argv[2];
-const ActorId = 18;
-const TargetLink = `https://swapi-api.alx-tools.com/api/people/${ActorId}/`;
-
 request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  }
-  const data = JSON.parse(body);
-  const results = data.results;
-
+  if (error) { return; }
   let count = 0;
-
-  results.forEach((item) => {
-    if (item.characters.includes(TargetLink)) {
-      count++;
+  const result = JSON.parse(body).results;
+  for (const movie of result) {
+    for (const character of movie.characters) {
+      if (character.includes('/18/') || character.includes('/18')) {
+        count++;
+      }
     }
-  });
+  }
   console.log(count);
 });
