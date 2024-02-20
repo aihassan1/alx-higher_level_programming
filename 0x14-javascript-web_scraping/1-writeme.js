ded818884115acd16db55b628bbed7eaaccd0677
aihassan1 <abdosruor57@gmail.com>
2024-02-20 23:00:33 +0200

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const path = process.argv[2];
+const data = process.argv[3];
+const fs = require('fs');
+
+fs.writeFile(path, data, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+});
