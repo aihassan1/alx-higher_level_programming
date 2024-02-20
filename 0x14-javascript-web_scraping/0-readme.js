15780d8f9e4dd89249d9ef6b02740275ec46b849
aihassan1 <abdosruor57@gmail.com>
2024-02-20 22:55:24 +0200

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const path = process.argv[2];
 const fs = require('fs');
 
-const data = fs.readFile(path, 'utf-8', (err, data) => {
+fs.readFile(path, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   }
