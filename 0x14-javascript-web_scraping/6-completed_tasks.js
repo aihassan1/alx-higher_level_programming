0e7bee94366ee0a2a525e928338c41b4dc3e4533
aihassan1 <abdosruor57@gmail.com>
2024-02-21 04:33:19 +0200

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,40 +4,41 @@ const request = require('request');
 const url = process.argv[2];
 
 // Function to count the number of completed tasks for a given user ID
-function trueCounter(data, userid) {
+function trueCounter (data, userId) {
   let counter = 0;
   // Loop through each item in the data
   for (const item of data) {
     // Check if the item belongs to the specified user ID and if it's completed
-    if (item.userId === userid && item.completed === true) {
+    if (item.userId === userId && item.completed === true) {
       counter++;
     }
   }
   // Return an object with the user ID as the key and the count as the value
-  return { [userid]: counter };
+  return { [userId]: counter };
 }
 
 // Make a request to the provided URL
 request(url, (error, response, body) => {
+  // Handle errors
   if (error) {
-    // If there's an error, log it
-    console.log(error);
+    console.error('Error:', error);
+    return;
   }
 
   // Parse the response body into JSON format
-  const body_data = JSON.parse(body);
+  const bodyData = JSON.parse(body);
   // Initialize an empty object to store the results
-  const users_completed_tasks = {};
+  const usersCompletedTasks = {};
 
   // Iterate through each item in the response body data
-  for (const item of body_data) {
-    // Call the true_counter function to count completed tasks for the current user ID
-    const result = trueCounter(body_data, item.userId);
+  for (const item of bodyData) {
+    // Call the trueCounter function to count completed tasks for the current user ID
+    const result = trueCounter(bodyData, item.userId);
 
-    // Assign the result to the users_completed_tasks object
-    Object.assign(users_completed_tasks, result);
+    // Assign the result to the usersCompletedTasks object
+    Object.assign(usersCompletedTasks, result);
   }
 
   // Output the object containing the count of completed tasks for each user ID
-  console.log(users_completed_tasks);
+  console.log(usersCompletedTasks);
 });
