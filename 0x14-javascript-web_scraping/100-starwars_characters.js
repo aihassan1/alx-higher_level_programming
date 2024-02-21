fb82b2ede03fa1fde413b5886551c056697c035f
aihassan1 <abdosruor57@gmail.com>
2024-02-21 04:52:02 +0200

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,15 +2,14 @@
 
 const request = require('request');
 const filmId = process.argv[2];
-
 const url = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;
 
-// Make a request to the provided URL
-request(url, (error, response, body) => {
-  // Handle errors
-  if (error) {
-    console.error('Error:', error);
-    return;
+request(url, function (_err, _res, body) {
+  const characters = JSON.parse(body).characters;
+
+  for (let i = 0; i < characters.length; ++i) {
+    request(characters[i], function (_cErr, _cRes, cBody) {
+      console.log(JSON.parse(cBody).name);
+    });
   }
 });
-console.log(body);
